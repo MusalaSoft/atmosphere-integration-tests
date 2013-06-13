7df50e517f015934151b6bfe63bb13a89428952c
georgi.gaydarov <georgi.gaydarov@34708651-a5da-c34d-b20c-10ddab38317d>
2013-06-13 08:51:26 +0000

    Changeset : - changed running the emulator executable (in EmulatorManager) to be done in a separate thread to avoid blocking;
    Initial import :
    - AtmosphereIntegrationTests project;
    - GetUiXmlTest.java - integration test for AbstractWrapDevice.getUiXml();
    - AgentIntegrationEnvironmentCreator utility class;
    
    [http://jenkins.musala.com/r/5117/ $5117] see #2015 see #2019
    
    git-svn-id: https://trac.musala.com/ARDC/repo@8287 34708651-a5da-c34d-b20c-10ddab38317d

---
+++ test/com/musala/atmosphere/agent/devicewrapper/GetUiXmlTest.java
@@ -0,0 +1,82 @@
+package com.musala.atmosphere.agent.devicewrapper;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.musala.atmosphere.agent.AgentIntegrationEnvironmentCreator;
+import com.musala.atmosphere.commons.sa.DeviceParameters;
+import com.musala.atmosphere.commons.sa.IWrapDevice;
+import com.musala.atmosphere.commons.sa.exceptions.CommandFailedException;
+import com.musala.atmosphere.commons.sa.util.Pair;
+
+public class GetUiXmlTest
+{
+	private static final int RMI_PORT = 1989;
+
+	private AgentIntegrationEnvironmentCreator environment;
+
+	private static final Logger LOGGER = Logger.getLogger(GetUiXmlTest.class.getName());
+
+	private static final int EMULATOR_CREATION_DPI = 120;
+
+	private static final int EMULATOR_CREATION_RAM = 256;
+
+	private static final int EMULATOR_CREATION_RESOLUTION_H = 240;
+
+	private static final int EMULATOR_CREATION_RESOLUTION_W = 360;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		// TODO configure logger properly
+		LOGGER.setLevel(Level.ALL);
+		LOGGER.addHandler(new ConsoleHandler());
+
+		environment = new AgentIntegrationEnvironmentCreator(RMI_PORT);
+
+		if (environment.isDevicePresent() == false)
+		{
+			DeviceParameters emulatorCreationParameters = new DeviceParameters();
+			emulatorCreationParameters.setDpi(EMULATOR_CREATION_DPI);
+			emulatorCreationParameters.setRam(EMULATOR_CREATION_RAM);
+			emulatorCreationParameters.setResolution(new Pair<Integer, Integer>(EMULATOR_CREATION_RESOLUTION_H,
+																				EMULATOR_CREATION_RESOLUTION_W));
+
+			environment.startEmulator(emulatorCreationParameters);
+		}
+
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		environment.close();
+	}
+
+	@Test
+	public void getUiXmlTest() throws RemoteException, CommandFailedException, NotBoundException
+	{
+		IWrapDevice deviceWrapper = environment.getFirstAvailableDeviceWrapper();
+		environment.waitForDeviceOsToStart(deviceWrapper);
+
+		String uiXmlDump = deviceWrapper.getUiXml();
+
+		LOGGER.log(Level.INFO, "UI xml dump returned :\n" + uiXmlDump);
+		assertNotNull("ui xml dump response can never be 'null'.", uiXmlDump);
+		assertTrue(	"ui xml dump must start with a <hierarchy ..> tag.",
+					uiXmlDump.startsWith("<?xml version='1.0' encoding='UTF-8' standalone='yes' ?><hierarchy"));
+		assertTrue(	"ui xml dump must end with a closing tag for the <hierarchy ..> tag.",
+					uiXmlDump.endsWith("</hierarchy>"));
+	}
+
+}
