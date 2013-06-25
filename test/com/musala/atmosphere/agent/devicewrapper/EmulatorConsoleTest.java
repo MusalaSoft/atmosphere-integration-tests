2c2babf06606e2fa9d81ab7fa9702b39d8761b87
vladimir.vladimirov <vladimir.vladimirov@34708651-a5da-c34d-b20c-10ddab38317d>
2013-06-25 11:19:51 +0000

    Initial import: - Integration test for implemented methods in EmulatorConsole;
    Changeset:
    - Added methods to the integration environment creator utility class for convenience (easier device selection).
    
    
    [http://jenkins.musala.com/r/5135/ $5135] see #2016
    
    git-svn-id: https://trac.musala.com/ARDC/repo@8358 34708651-a5da-c34d-b20c-10ddab38317d

---
+++ test/com/musala/atmosphere/agent/devicewrapper/EmulatorConsoleTest.java
@@ -0,0 +1,105 @@
+package com.musala.atmosphere.agent.devicewrapper;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.musala.atmosphere.agent.AgentIntegrationEnvironmentCreator;
+import com.musala.atmosphere.commons.CommandFailedException;
+import com.musala.atmosphere.commons.Pair;
+import com.musala.atmosphere.commons.sa.DeviceInformation;
+import com.musala.atmosphere.commons.sa.DeviceParameters;
+import com.musala.atmosphere.commons.sa.IWrapDevice;
+
+public class EmulatorConsoleTest
+{
+	private static final int RMI_PORT = 1989;
+
+	private AgentIntegrationEnvironmentCreator environment;
+
+	private static final int EMULATOR_CREATION_DPI = 120;
+
+	private static final int EMULATOR_CREATION_RAM = 256;
+
+	private static final int EMULATOR_CREATION_RESOLUTION_H = 240;
+
+	private static final int EMULATOR_CREATION_RESOLUTION_W = 360;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		environment = new AgentIntegrationEnvironmentCreator(RMI_PORT);
+
+		if (environment.isAnyEmulatorPresent() == false)
+		{
+			DeviceParameters emulatorCreationParameters = new DeviceParameters();
+			emulatorCreationParameters.setDpi(EMULATOR_CREATION_DPI);
+			emulatorCreationParameters.setRam(EMULATOR_CREATION_RAM);
+			emulatorCreationParameters.setResolution(new Pair<Integer, Integer>(EMULATOR_CREATION_RESOLUTION_H,
+																				EMULATOR_CREATION_RESOLUTION_W));
+
+			environment.startEmulator(emulatorCreationParameters);
+		}
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		environment.close();
+	}
+
+	@Test
+	public void testSetNetworkSpeed()
+	{
+		// TODO method setNetworkSpeed() is not implemented for devices
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBatteryLevel()
+		throws RemoteException,
+			IllegalStateException,
+			NotBoundException,
+			CommandFailedException,
+			InterruptedException
+	{
+		final int initialBatteryLevel = 20;
+		final int batteryLevel = 80;
+
+		IWrapDevice deviceWrapper = environment.getFirstAvailableEmulatorDeviceWrapper();
+		environment.waitForDeviceOsToStart(deviceWrapper);
+
+		DeviceInformation deviceInformation = deviceWrapper.getDeviceInformation();
+		assertTrue("Device must be an emulator.", deviceInformation.isEmulator());
+
+		deviceWrapper.setBatteryLevel(initialBatteryLevel);
+		int newBatteryLevel = deviceWrapper.getBatteryLevel();
+		assertEquals("Battery level doesn't match.", initialBatteryLevel, newBatteryLevel);
+
+		deviceWrapper.setBatteryLevel(batteryLevel);
+		int newBatteryLevel2 = deviceWrapper.getBatteryLevel();
+		assertEquals("Battery level doesn't match.", batteryLevel, newBatteryLevel2);
+	}
+
+	@Test
+	public void testSetNetworkLatency()
+	{
+		// TODO method setNetworkLatency() is not implemented for device
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBatteryState()
+	{
+		// TODO method setBatteryState() is not implemented for device
+		fail("Not yet implemented");
+	}
+
+}
