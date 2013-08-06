51388d2a08b29c38c00ee97e1d36ddb330789108
vladimir.vladimirov <vladimir.vladimirov@34708651-a5da-c34d-b20c-10ddab38317d>
2013-08-06 11:02:41 +0000

    Changeset:  - Implemented method byte[] Device.getScreenshot();  - Implemented method void Device.getScreenshot(String pathToScreenshot);
    Initial import:
     - Included integration test for the implemented methods in class Device.
    
    [http://jenkins.musala.com/r/5387/ $5387] see #2265
    
    git-svn-id: https://trac.musala.com/ARDC/repo@8569 34708651-a5da-c34d-b20c-10ddab38317d

---
+++ test/com/musala/atmosphere/client/device/GetScreenshotTest.java
@@ -0,0 +1,138 @@
+package com.musala.atmosphere.client.device;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.musala.atmosphere.agent.AgentIntegrationEnvironmentCreator;
+import com.musala.atmosphere.client.Builder;
+import com.musala.atmosphere.client.Device;
+import com.musala.atmosphere.client.Screen;
+import com.musala.atmosphere.client.UiElement;
+import com.musala.atmosphere.client.util.Server;
+import com.musala.atmosphere.commons.Pair;
+import com.musala.atmosphere.commons.cs.clientbuilder.DeviceParameters;
+import com.musala.atmosphere.commons.cs.clientbuilder.DeviceType;
+import com.musala.atmosphere.commons.sa.IWrapDevice;
+import com.musala.atmosphere.server.ServerIntegrationEnvironmentCreator;
+
+public class GetScreenshotTest
+{
+	private static final Logger LOGGER = Logger.getLogger(GetScreenshotTest.class.getCanonicalName());
+
+	private final static int POOLMANAGER_RMI_PORT = 2200;
+
+	private final static int AGENTMANAGER_RMI_PORT = 2300;
+
+	private static final String PATH_TO_SCREENSHOT = "./Screenshot.png";
+
+	/**
+	 * {@value #WIDGET_DRAWING_TIMEOUT} milliseconds
+	 */
+	private static final long WIDGET_DRAWING_TIMEOUT = 2000;
+
+	private AgentIntegrationEnvironmentCreator agentEnvironment;
+
+	private ServerIntegrationEnvironmentCreator serverEnvironment;
+
+	@Server(ip = "localhost", port = POOLMANAGER_RMI_PORT)
+	class DeviceTestClass
+	{
+		private Device device = null;
+
+		public DeviceTestClass(DeviceParameters parameters)
+		{
+			device = Builder.getInstance().getDevice(parameters);
+		}
+
+		public Device getSelectedDevice()
+		{
+			return device;
+		}
+	}
+
+	@Before
+	public void setUp() throws Exception
+	{
+		// TODO extract emulator creation logic in separate utility class
+		agentEnvironment = new AgentIntegrationEnvironmentCreator(AGENTMANAGER_RMI_PORT);
+		serverEnvironment = new ServerIntegrationEnvironmentCreator(POOLMANAGER_RMI_PORT);
+		agentEnvironment.connectToLocalhostServer(POOLMANAGER_RMI_PORT);
+		String agentId = agentEnvironment.getUnderlyingAgentId();
+		serverEnvironment.waitForAgentConnection(agentId);
+
+		if (!agentEnvironment.isAnyDevicePresent())
+		{
+			com.musala.atmosphere.commons.sa.DeviceParameters emulatorCreationParameters = new com.musala.atmosphere.commons.sa.DeviceParameters();
+			emulatorCreationParameters.setResolution(new Pair<Integer, Integer>(120, 160));
+			emulatorCreationParameters.setRam(1024);
+			emulatorCreationParameters.setDpi(96);
+
+			IWrapDevice createdEmulatorWrapper = agentEnvironment.startEmulator(emulatorCreationParameters);
+		}
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		serverEnvironment.close();
+		agentEnvironment.close();
+	}
+
+	@Test
+	public void getScreenShotTest()
+	{
+		DeviceParameters selectionParameters = new DeviceParameters();
+		selectionParameters.setDeviceType(DeviceType.EMULATOR_ONLY);
+		DeviceTestClass testClass = new DeviceTestClass(selectionParameters);
+
+		Device testDevice = testClass.getSelectedDevice();
+		waitForDeviceToBeLoaded(testDevice);
+
+		// getting screenshot without dumping it to file
+		byte[] screenshot = testDevice.getScreenshot();
+		assertTrue("Getting screenshot returned 'null'", (screenshot != null));
+
+		// getting screenshot with dumping it to file
+		testDevice.getScreenshot(PATH_TO_SCREENSHOT);
+		File dumpedScreenshot = new File(PATH_TO_SCREENSHOT);
+		assertTrue("Screenshot is not dumped as image!", dumpedScreenshot.exists());
+		dumpedScreenshot.delete();
+	}
+
+	// waits for the Android to be loaded by checking if there are any clickable elements
+	// TODO this should be extracted to a utility class
+	private static void waitForDeviceToBeLoaded(Device testDevice)
+	{
+		final int MAX_TRY_COUNT = 123456;
+
+		for (int numberOfTry = 1; numberOfTry <= MAX_TRY_COUNT; numberOfTry++)
+		{
+			try
+			{
+				Screen deviceScreen = testDevice.getActiveScreen();
+				UiElement someClickableElement = deviceScreen.getElementXPath("//*[(@clickable='true' or @long-clickable='true') and @enabled='true']");
+				// if no screen is fetched or no clickable UI element is present, we are thrown an exception and assume
+				// the emulator has not loaded; otherwise we assume the emulator OS has loaded
+				break;
+			}
+			catch (Throwable throwable)
+			{
+			}
+		}
+		// just in case we wait some time for drawing the UI elements after everything is loaded
+		try
+		{
+			Thread.sleep(WIDGET_DRAWING_TIMEOUT);
+		}
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
