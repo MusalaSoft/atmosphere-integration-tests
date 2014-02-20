306c18ce460984914c3603139e9b914ab48efba6
yavor.stankov <yavor.stankov@34708651-a5da-c34d-b20c-10ddab38317d>
2014-02-20 13:51:39 +0000

    Changeset : * Disable setting airplane mode for emulators.
    [http://jenkins.musala.com/r/6910/ $6910] see #2760
    
    
    git-svn-id: https://trac.musala.com/ARDC/repo@9686 34708651-a5da-c34d-b20c-10ddab38317d

--- test/com/musala/atmosphere/agent/devicewrapper/AirplaneModeTest.java
+++ test/com/musala/atmosphere/agent/devicewrapper/AirplaneModeTest.java
@@ -2,6 +2,9 @@ package com.musala.atmosphere.agent.devicewrapper;
 
 import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidatorAssert.assertInAirplaneMode;
 import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidatorAssert.assertNotInAirplaneMode;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -9,31 +12,35 @@ import org.junit.Test;
 import com.musala.atmosphere.BaseIntegrationTest;
 import com.musala.atmosphere.commons.cs.clientbuilder.DeviceParameters;
 
-public class AirplaneModeTest extends BaseIntegrationTest
-{
-	@BeforeClass
-	public static void setUp() throws Exception
-	{
-		initTestDevice(new DeviceParameters());
-		installValidatorApplication();
-		testDevice.startActivity(VALIDATOR_APP_PACKAGE, VALIDATOR_APP_ACTIVITY, true);
-		Thread.sleep(1000);
-	}
+public class AirplaneModeTest extends BaseIntegrationTest {
+    @BeforeClass
+    public static void setUp() throws Exception {
+        initTestDevice(new DeviceParameters());
+        installValidatorApplication();
+        testDevice.startActivity(VALIDATOR_APP_PACKAGE, VALIDATOR_APP_ACTIVITY, true);
+        Thread.sleep(1000);
+    }
 
-	@Test
-	public void testSetAirplaneModeOn() throws Exception
-	{
-		// set airplane mode on
-		testDevice.setAirplaneMode(true);
-		assertInAirplaneMode("Airplane mode not set to the expected value.");
-	}
+    @Test
+    public void testSetAirplaneModeOn() throws Exception {
+        // set airplane mode on
+        assumeTrue(!testDevice.getInformation().isEmulator());
+        assertTrue("Setting airplane mode returned false.", testDevice.setAirplaneMode(true));
+        assertInAirplaneMode("Airplane mode not set to the expected value.");
+    }
 
-	@Test
-	public void testSetAirplaneModeOff() throws Exception
-	{
+    @Test
+    public void testSetAirplaneModeOff() throws Exception {
+        // set airplane mode off
+        assumeTrue(!testDevice.getInformation().isEmulator());
+        assertTrue("Setting airplane mode returned false.", testDevice.setAirplaneMode(false));
+        assertNotInAirplaneMode("Airplane mode not set to the expected value.");
+    }
 
-		// set airplane mode off
-		testDevice.setAirplaneMode(false);
-		assertNotInAirplaneMode("Airplane mode not set to the expected value.");
-	}
+    @Test
+    public void testSetAirplaneModeOnEmulator() throws Exception {
+        assumeTrue(testDevice.getInformation().isEmulator());
+        assertFalse("Setting airplane mode returned true.", testDevice.setAirplaneMode(true));
+        assertFalse("Setting airplane mode returned true.", testDevice.setAirplaneMode(false));
+    }
 }
