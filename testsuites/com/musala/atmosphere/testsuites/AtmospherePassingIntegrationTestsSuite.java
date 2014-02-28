984b258bfa6f35bbb319b5a0b5be738bd743c572
georgi.gaydarov <georgi.gaydarov@34708651-a5da-c34d-b20c-10ddab38317d>
2014-02-28 10:49:25 +0000

    Missed files from previous commit. related to #2797, #2798
    git-svn-id: https://trac.musala.com/ARDC/repo@9716 34708651-a5da-c34d-b20c-10ddab38317d

--- testsuites/com/musala/atmosphere/testsuites/AtmospherePassingIntegrationTestsSuite.java
+++ testsuites/com/musala/atmosphere/testsuites/AtmospherePassingIntegrationTestsSuite.java
@@ -3,26 +3,11 @@ package com.musala.atmosphere.testsuites;
 import org.junit.runners.Suite.SuiteClasses;
 
 import com.musala.atmosphere.agent.devicewrapper.BatteryRelatedMethodsTest;
-import com.musala.atmosphere.agent.devicewrapper.GetScreenshotTest;
-import com.musala.atmosphere.agent.devicewrapper.GetUiXmlTest;
-import com.musala.atmosphere.agent.devicewrapper.ScreenOrientationTest;
-import com.musala.atmosphere.agent.devicewrapper.TelephonyInformationTest;
-import com.musala.atmosphere.client.builder.BuilderDeviceSelectionIntegrationTest;
-import com.musala.atmosphere.client.device.CallTest;
-import com.musala.atmosphere.client.device.DeviceGetScreenshotTest;
-import com.musala.atmosphere.client.device.DeviceInstallApkTest;
-import com.musala.atmosphere.client.device.GestureExecutionTest;
-import com.musala.atmosphere.client.device.LockUnlockTest;
-import com.musala.atmosphere.client.device.ReceiveSmsTest;
-import com.musala.atmosphere.client.device.StartActivityTest;
-import com.musala.atmosphere.client.device.TapTest;
-import com.musala.atmosphere.server.PoolEventHandlerTest;
 
-@SuiteClasses({ScreenOrientationTest.class, PoolEventHandlerTest.class, BatteryRelatedMethodsTest.class, TapTest.class,
-		LockUnlockTest.class, StartActivityTest.class, BuilderDeviceSelectionIntegrationTest.class, GetUiXmlTest.class,
-		DeviceInstallApkTest.class, GetScreenshotTest.class, DeviceGetScreenshotTest.class, ReceiveSmsTest.class,
-		CallTest.class, GestureExecutionTest.class, TelephonyInformationTest.class})
-// @SuiteClasses({TelephonyInformationTest.class})
-public class AtmospherePassingIntegrationTestsSuite extends AtmosphereIntegrationTestsSuite
-{
+//@SuiteClasses({ScreenOrientationTest.class, PoolEventHandlerTest.class, BatteryRelatedMethodsTest.class, TapTest.class,
+//		LockUnlockTest.class, StartActivityTest.class, BuilderDeviceSelectionIntegrationTest.class, GetUiXmlTest.class,
+//		DeviceInstallApkTest.class, GetScreenshotTest.class, DeviceGetScreenshotTest.class, ReceiveSmsTest.class,
+//		CallTest.class, GestureExecutionTest.class, TelephonyInformationTest.class})
+@SuiteClasses({BatteryRelatedMethodsTest.class})
+public class AtmospherePassingIntegrationTestsSuite extends AtmosphereIntegrationTestsSuite {
 }
