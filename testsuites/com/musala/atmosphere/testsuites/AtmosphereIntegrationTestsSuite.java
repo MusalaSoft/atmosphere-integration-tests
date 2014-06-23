c5f24a56272aec93e1fc71bf834b4a2c5c8a75fa
yavor.stankov <yavor.stankov@34708651-a5da-c34d-b20c-10ddab38317d>
2014-06-23 10:03:28 +0000

    Changeset: * Create builder in AtmosphereIntegrationTestSuite() class. * Get the available devices. * Setup OnDeviceValidator on them.
    [http://jenkins.musala.com/r/8858/ $8858] see #3308
    
    git-svn-id: https://trac.musala.com/ARDC/repo@10155 34708651-a5da-c34d-b20c-10ddab38317d

--- testsuites/com/musala/atmosphere/testsuites/AtmosphereIntegrationTestsSuite.java
+++ testsuites/com/musala/atmosphere/testsuites/AtmosphereIntegrationTestsSuite.java
@@ -6,7 +6,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import com.musala.atmosphere.agent.Agent;
+import com.musala.atmosphere.client.Builder;
+import com.musala.atmosphere.client.Device;
+import com.musala.atmosphere.commons.cs.clientbuilder.DeviceParameters;
 import com.musala.atmosphere.server.Server;
+import com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidatorAssert;
 
 /**
  * JUnit test suite with all running integration tests.
@@ -20,9 +24,24 @@ public class AtmosphereIntegrationTestsSuite {
 
     private final static int AGENTMANAGER_RMI_PORT = 2000;
 
+    private final static int WAIT_FOR_CONNECTING_TIMEOUT = 3000;
+
+    private final static String SERVER_IP = "localhost";
+
     private static Agent agent;
 
     private static Server server;
+
+    @com.musala.atmosphere.client.util.Server(ip = SERVER_IP, port = SERVER_MANAGER_RMI_PORT, connectionRetryLimit = 0)
+    private static class GettingBuilderClass {
+        public GettingBuilderClass() {
+        }
+
+        public Builder getBuilder() {
+            Builder classDeviceBuilder = Builder.getInstance();
+            return classDeviceBuilder;
+        }
+    }
 
     @BeforeClass
     public static void setUpClass() throws Exception {
@@ -30,13 +49,23 @@ public class AtmosphereIntegrationTestsSuite {
         server = new Server(SERVER_MANAGER_RMI_PORT);
         server.run();
 
-        // Start and connect agent to the server
+        // Start and connect agent to the server.
         agent = new Agent(AGENTMANAGER_RMI_PORT);
-        agent.connectToServer("localhost", SERVER_MANAGER_RMI_PORT);
+        agent.connectToServer(SERVER_IP, SERVER_MANAGER_RMI_PORT);
         String agentId = agent.getId();
         server.waitForGivenAgentToConnect(agentId);
+        Thread.sleep(WAIT_FOR_CONNECTING_TIMEOUT);
 
-        Thread.sleep(10000);
+        // Install OnDeviceValidator on all available devices.
+        GettingBuilderClass builderGet = new GettingBuilderClass();
+        Builder deviceBuilder = builderGet.getBuilder();
+
+        Device device = deviceBuilder.getDevice(new DeviceParameters());
+
+        OnDeviceValidatorAssert.setTestDevice(device);
+        OnDeviceValidatorAssert.setupOndeviceValidator();
+
+        deviceBuilder.releaseAllDevices();
     }
 
     @AfterClass
