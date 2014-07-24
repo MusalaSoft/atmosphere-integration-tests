c111481c5b28f6e8be9c0c67837a7b50ddf03545
yavor.stankov <yavor.stankov@34708651-a5da-c34d-b20c-10ddab38317d>
2014-07-24 08:31:23 +0000

    Changeset:
    Add method for setting and getting time in TimePicker.
    Add method for setting text in an edit text field in the PickerHelper.
    Add integration tests for the implemented functionality.
    
    [https://inttools.musala.com/reviewboard/r/23/ $23] see #3453
    
    git-svn-id: https://trac.musala.com/ARDC/repo@10287 34708651-a5da-c34d-b20c-10ddab38317d

--- test/com/musala/atmosphere/client/device/TimePickerInteractionTest.java
+++ test/com/musala/atmosphere/client/device/TimePickerInteractionTest.java
@@ -4,11 +4,13 @@ import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidato
 import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidatorAssert.setTestDevice;
 import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidatorAssert.startPickerActivity;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Calendar;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -16,6 +18,9 @@ import com.musala.atmosphere.BaseIntegrationTest;
 import com.musala.atmosphere.client.Screen;
 import com.musala.atmosphere.client.TimePicker;
 import com.musala.atmosphere.client.UiElement;
+import com.musala.atmosphere.client.uiutils.CssAttribute;
+import com.musala.atmosphere.client.uiutils.UiElementSelector;
+import com.musala.atmosphere.client.util.settings.ElementValidationType;
 import com.musala.atmosphere.commons.cs.clientbuilder.DeviceParameters;
 import com.musala.atmosphere.commons.cs.clientbuilder.DeviceType;
 import com.musala.atmosphere.test.util.ondevicevalidator.ContentDescriptor;
@@ -25,7 +30,15 @@ public class TimePickerInteractionTest extends BaseIntegrationTest {
 
     private static final int EXPECTED_MINUTE = 45;
 
-    private static final int EXPECTED_MERIDIEM = Calendar.AM;
+    private static final int HOUR_TO_SET = 21;
+
+    private static int MINUTE_TO_SET = 32;
+
+    private static final int AM = Calendar.AM;
+
+    private static final int PM = Calendar.PM;
+
+    private static final String DONE = "Done";
 
     @BeforeClass
     public static void setUp() throws Exception {
@@ -33,7 +46,6 @@ public class TimePickerInteractionTest extends BaseIntegrationTest {
         testDeviceParams.setDeviceType(DeviceType.DEVICE_PREFERRED);
         initTestDevice(testDeviceParams);
         setTestDevice(testDevice);
-        startPickerActivity();
     }
 
     @AfterClass
@@ -42,9 +54,14 @@ public class TimePickerInteractionTest extends BaseIntegrationTest {
         releaseDevice();
     }
 
+    @Before
+    public void setUpTest() throws Exception {
+        startPickerActivity();
+    }
+
     @After
     public void tearDownTest() {
-        testDevice.pressButton(HardwareButton.BACK);
+        testDevice.forceStopProcess(VALIDATOR_APP_PACKAGE);
     }
 
     @Test
@@ -55,15 +72,14 @@ public class TimePickerInteractionTest extends BaseIntegrationTest {
         Screen screen = testDevice.getActiveScreen();
         TimePicker timePicker = screen.getTimePicker();
         Calendar calendar = timePicker.getValue();
+
         assertEquals("Hour from time picker does not match expected result.",
-                     calendar.get(Calendar.HOUR),
-                     EXPECTED_HOUR);
+                     EXPECTED_HOUR,
+                     calendar.get(Calendar.HOUR));
         assertEquals("Minute from time picker does not match expected result.",
-                     calendar.get(Calendar.MINUTE),
-                     EXPECTED_MINUTE);
-        assertEquals("Meridiem from time picker does not match expected result.",
-                     calendar.get(Calendar.AM_PM),
-                     EXPECTED_MERIDIEM);
+                     EXPECTED_MINUTE,
+                     calendar.get(Calendar.MINUTE));
+        assertEquals("Meridiem from time picker does not match expected result.", AM, calendar.get(Calendar.AM_PM));
     }
 
     @Test
@@ -74,12 +90,103 @@ public class TimePickerInteractionTest extends BaseIntegrationTest {
         Screen screen = testDevice.getActiveScreen();
         TimePicker timePicker = screen.getTimePicker();
         Calendar calendar = timePicker.getValue();
+
         assertEquals("Hour from time picker does not match expected result.",
-                     calendar.get(Calendar.HOUR),
-                     EXPECTED_HOUR);
+                     EXPECTED_HOUR,
+                     calendar.get(Calendar.HOUR));
         assertEquals("MINUTE from time picker does not match expected result.",
-                     calendar.get(Calendar.MINUTE),
-                     EXPECTED_MINUTE);
+                     EXPECTED_MINUTE,
+                     calendar.get(Calendar.MINUTE));
+    }
+
+    @Test
+    public void testSetTimeInTimePicker() throws Exception {
+        UiElement timePickerButton = getElementByContentDescriptor(ContentDescriptor.TIME_PICKER.toString());
+        timePickerButton.setValidationType(ElementValidationType.MANUAL);
+        timePickerButton.tap();
+
+        Screen screen = testDevice.getActiveScreen();
+        TimePicker timePicker = screen.getTimePicker();
+
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(Calendar.HOUR_OF_DAY, HOUR_TO_SET);
+        calendar.set(Calendar.MINUTE, MINUTE_TO_SET);
+
+        boolean result = timePicker.setValue(calendar);
+
+        assertTrue("Setting time in a time picker failed", result);
+
+        clickDone(screen);
+
+        screen.updateScreen();
+
+        timePickerButton = getElementByContentDescriptor(ContentDescriptor.TIME_PICKER.toString());
+        timePickerButton.tap();
+
+        screen.updateScreen();
+
+        timePicker = screen.getTimePicker();
+        Calendar pickerCalendar = timePicker.getValue();
+
+        assertEquals("Hour from time picker does not match expected result.",
+                     HOUR_TO_SET,
+                     pickerCalendar.get(Calendar.HOUR_OF_DAY));
+        assertEquals("Minute from time picker does not match expected result.",
+                     MINUTE_TO_SET,
+                     pickerCalendar.get(Calendar.MINUTE));
+        assertEquals("Meridiem from time picker does not match expected result.",
+                     PM,
+                     pickerCalendar.get(Calendar.AM_PM));
+    }
+
+    @Test
+    public void testSetTimeInTimePicker24HoursFormat() throws Exception {
+        UiElement timePickerButton = getElementByContentDescriptor(ContentDescriptor.TIME_PICKER_24_HOURS_FORMAT.toString());
+        timePickerButton.tap();
+
+        Screen screen = testDevice.getActiveScreen();
+        TimePicker timePicker = screen.getTimePicker();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, HOUR_TO_SET);
+        calendar.set(Calendar.MINUTE, MINUTE_TO_SET);
+
+        boolean result = timePicker.setValue(calendar);
+
+        assertTrue("Setting time in a time picker failed", result);
+
+        clickDone(screen);
+
+        screen.updateScreen();
+
+        timePickerButton = getElementByContentDescriptor(ContentDescriptor.TIME_PICKER_24_HOURS_FORMAT.toString());
+        timePickerButton.tap();
+
+        screen.updateScreen();
+
+        timePicker = screen.getTimePicker();
+        Calendar pickerCalendar = timePicker.getValue();
+
+        assertEquals("Hour from time picker does not match expected result.",
+                     HOUR_TO_SET,
+                     pickerCalendar.get(Calendar.HOUR_OF_DAY));
+        assertEquals("Minute from time picker does not match expected result.",
+                     MINUTE_TO_SET,
+                     pickerCalendar.get(Calendar.MINUTE));
+    }
+
+    /**
+     * Press the button done of the time picker.
+     * 
+     * @param screen
+     * @throws Exception
+     */
+    private void clickDone(Screen screen) throws Exception {
+        UiElementSelector doneButtonSelector = new UiElementSelector();
+        doneButtonSelector.addSelectionAttribute(CssAttribute.TEXT, DONE);
+        UiElement doneButton = screen.getElement(doneButtonSelector);
+        doneButton.tap();
     }
 
 }
