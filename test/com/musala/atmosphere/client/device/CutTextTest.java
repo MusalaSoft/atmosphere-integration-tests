2398b31eca85402335769077d49a4f4c16658fb9
yavor.stankov <yavor.stankov@34708651-a5da-c34d-b20c-10ddab38317d>
2015-02-23 13:10:21 +0000

    Changeset:  - Add custom cut method in Atmosphere IME;  - Add cut method in the Device;  - Add cut method in the UiElement;  - Remove all unused constants from the Device. Iinitia import:  - Integration tests for the new cut functionality.
    [https://inttools.musala.com/reviewboard/r/659/ $659] see #4439
    
    git-svn-id: https://trac.musala.com/ARDC/repo@11581 34708651-a5da-c34d-b20c-10ddab38317d

---
+++ test/com/musala/atmosphere/client/device/CutTextTest.java
@@ -0,0 +1,151 @@
+package com.musala.atmosphere.client.device;
+
+import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidatorAssert.assertElementText;
+import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidatorAssert.getElementByContentDescriptor;
+import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidatorAssert.setTestDevice;
+import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidatorAssert.startImeTestActivity;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.musala.atmosphere.BaseIntegrationTest;
+import com.musala.atmosphere.client.Screen;
+import com.musala.atmosphere.client.UiElement;
+import com.musala.atmosphere.client.geometry.Point;
+import com.musala.atmosphere.client.uiutils.CssAttribute;
+import com.musala.atmosphere.client.uiutils.UiElementSelector;
+import com.musala.atmosphere.commons.cs.clientbuilder.DeviceParameters;
+import com.musala.atmosphere.test.util.ondevicevalidator.ContentDescriptor;
+
+/**
+ * 
+ * @author yavor.stankov
+ * 
+ */
+public class CutTextTest extends BaseIntegrationTest {
+    private static final String CUT_TEXT_FAILED_MESSAGE = "Cut text failed! The text field is not empty.";
+
+    private static final String PASTE_TEXT_FAILED_MESSAGE = "Paste text failed! The text field content does not match the expected one.";
+
+    private static final String PASTE_BUTTON_CONTENT_DESCRIPTOR = "Paste";
+
+    private static final String SELECT_ALL_BUTTON_CONTENT_DESCRIPTOR = "Select all";
+
+    private static final int LONG_PRESS_TIMEOUT = 1500;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        initTestDevice(new DeviceParameters());
+        setTestDevice(testDevice);
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        releaseDevice();
+    }
+
+    @Before
+    public void setUpTest() throws Exception {
+        startImeTestActivity();
+    }
+
+    @After
+    public void tearDownTest() {
+        testDevice.forceStopProcess(VALIDATOR_APP_PACKAGE);
+    }
+
+    @Test
+    public void testCutTextSingleLine() throws Exception {
+        Screen screen = testDevice.getActiveScreen();
+
+        UiElement singleLineTextBox = getElementByContentDescriptor(ContentDescriptor.CONTENT_TEXT_BOX.toString());
+        UiElementSelector singleLineTextBoxSelector = singleLineTextBox.getElementSelector();
+
+        String expectedText = singleLineTextBoxSelector.getStringValue(CssAttribute.TEXT);
+
+        singleLineTextBox.selectAllText();
+
+        singleLineTextBox.cutText();
+
+        screen.updateScreen();
+
+        singleLineTextBox = getElementByContentDescriptor(ContentDescriptor.CONTENT_TEXT_BOX.toString());
+
+        assertElementText(CUT_TEXT_FAILED_MESSAGE, singleLineTextBox, null);
+
+        UiElement pasteTextBox = getElementByContentDescriptor(ContentDescriptor.PASTE_CONTAINER_TEXT_BOX.toString());
+
+        Point pasteTextBoxBeginning = new Point(0, 0);
+        pasteTextBox.longPress(pasteTextBoxBeginning, LONG_PRESS_TIMEOUT);
+
+        screen.updateScreen();
+
+        UiElementSelector selectAllButtonSelector = new UiElementSelector();
+        selectAllButtonSelector.addSelectionAttribute(CssAttribute.CONTENT_DESCRIPTION,
+                                                      SELECT_ALL_BUTTON_CONTENT_DESCRIPTOR);
+
+        UiElement selectAllButton = screen.getElement(selectAllButtonSelector);
+        selectAllButton.tap();
+
+        UiElementSelector pasteButtonSelector = new UiElementSelector();
+        pasteButtonSelector.addSelectionAttribute(CssAttribute.CONTENT_DESCRIPTION, PASTE_BUTTON_CONTENT_DESCRIPTOR);
+
+        UiElement pasteButton = screen.getElement(pasteButtonSelector);
+        pasteButton.tap();
+
+        screen.updateScreen();
+
+        pasteTextBox = getElementByContentDescriptor(ContentDescriptor.PASTE_CONTAINER_TEXT_BOX.toString());
+
+        assertElementText(PASTE_TEXT_FAILED_MESSAGE, pasteTextBox, expectedText);
+    }
+
+    @Test
+    public void testCutTextMultiline() throws Exception {
+        Screen screen = testDevice.getActiveScreen();
+
+        UiElement multilineTextBox = getElementByContentDescriptor(ContentDescriptor.EMPTY_FIRST_LINE_TEXT_BOX.toString());
+        UiElementSelector multilineTextBoxSelector = multilineTextBox.getElementSelector();
+
+        String expectedText = multilineTextBoxSelector.getStringValue(CssAttribute.TEXT);
+
+        multilineTextBox.selectAllText();
+
+        multilineTextBox.cutText();
+
+        screen.updateScreen();
+
+        multilineTextBox = getElementByContentDescriptor(ContentDescriptor.EMPTY_FIRST_LINE_TEXT_BOX.toString());
+
+        assertElementText(CUT_TEXT_FAILED_MESSAGE, multilineTextBox, null);
+
+        UiElement pasteTextBox = getElementByContentDescriptor(ContentDescriptor.PASTE_CONTAINER_TEXT_BOX.toString());
+
+        Point pasteTextBoxBeginning = new Point(0, 0);
+        pasteTextBox.longPress(pasteTextBoxBeginning, LONG_PRESS_TIMEOUT);
+
+        screen.updateScreen();
+
+        UiElementSelector selectAllButtonSelector = new UiElementSelector();
+        selectAllButtonSelector.addSelectionAttribute(CssAttribute.CONTENT_DESCRIPTION,
+                                                      SELECT_ALL_BUTTON_CONTENT_DESCRIPTOR);
+
+        UiElement selectAllButton = screen.getElement(selectAllButtonSelector);
+        selectAllButton.tap();
+
+        UiElementSelector pasteButtonSelector = new UiElementSelector();
+        pasteButtonSelector.addSelectionAttribute(CssAttribute.CONTENT_DESCRIPTION, PASTE_BUTTON_CONTENT_DESCRIPTOR);
+
+        UiElement pasteButton = screen.getElement(pasteButtonSelector);
+        pasteButton.tap();
+
+        screen.updateScreen();
+
+        pasteTextBox = getElementByContentDescriptor(ContentDescriptor.PASTE_CONTAINER_TEXT_BOX.toString());
+
+        assertElementText(PASTE_TEXT_FAILED_MESSAGE, pasteTextBox, expectedText);
+    }
+}
