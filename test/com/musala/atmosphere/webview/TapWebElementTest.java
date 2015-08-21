3565de0dd59eccfcb456d85a862ab2f9d49c31d1
filareta.yordanova <filareta.yordanova@musala.com>
2015-08-21 19:14:12 +0300

    Store Proper Selection Criterion When Finding List of Web Elements
    see #123
    
    Change-Id: I0a8018763c8ffbf0e4fe1b36d82cc11098bb6a72
    Conflicts:
    	ARDC/Source/ATMOSPHERE/OnDeviceValidator/assets/web_view_sample.html

--- test/com/musala/atmosphere/webview/TapWebElementTest.java
+++ test/com/musala/atmosphere/webview/TapWebElementTest.java
@@ -4,6 +4,8 @@ import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidato
 import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidatorAssert.startWebViewActivity;
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -23,11 +25,19 @@ import com.musala.atmosphere.commons.webelement.selection.WebElementSelectionCri
  *
  */
 public class TapWebElementTest extends BaseIntegrationTest {
+    private static final String ATTRIBUTE_VALUE_MISSMATCH_ERROR_MESSAGE = "Expected attribute value does not match the received one.";
+
+    private static final String TAP_FAILED_MESSAGE = "Failed to tap the web element.";
+
+    private static final String EXISTING_WEB_ELEMENT_TAG = "button";
+
     private static final String WEB_ELEMENT_ID = "btn";
 
     private static final String DYNAMIC_ATTRIBUTE_NAME = "testAttribute";
 
-    private static final String DYNAMIC_ATTRIBUTE_EXPECTED_VALUE = "tapped";
+    private static final String SUBMIT_BUTTON_DYNAMIC_ATTRIBUTE_VALUE = "tappedSubmit";
+
+    private static final String CANCEL_BUTTON_DYNAMIC_ATTRIBUTE_VALUE = "tappedCancel";
 
     private static Screen screen;
 
@@ -60,6 +70,35 @@ public class TapWebElementTest extends BaseIntegrationTest {
 
         String dynamicAtributeValue = (String) webElement.getAttribute(DYNAMIC_ATTRIBUTE_NAME);
 
-        assertEquals("Failed to tap the web element.", dynamicAtributeValue, DYNAMIC_ATTRIBUTE_EXPECTED_VALUE);
+        assertEquals(TAP_FAILED_MESSAGE, SUBMIT_BUTTON_DYNAMIC_ATTRIBUTE_VALUE, dynamicAtributeValue);
+    }
+
+    @Test
+    public void testTapWebElementInList() {
+        String[] expectedResult = {"submit", "cancel", "getSizeTest"};
+        List<UiWebElement> webElements = webView.findElements(WebElementSelectionCriterion.TAG,
+                                                              EXISTING_WEB_ELEMENT_TAG);
+
+        assertEquals("Number of expected web elements found does not match the actual one.",
+                     expectedResult.length,
+                     webElements.size());
+        int index = 0;
+        for (UiWebElement webElement : webElements) {
+            assertEquals(ATTRIBUTE_VALUE_MISSMATCH_ERROR_MESSAGE,
+                         expectedResult[index],
+                         webElement.getAttribute("name"));
+            index++;
+        }
+
+        UiWebElement closeButton = webElements.get(1);
+        closeButton.tap();
+
+        String dynamicAtributeValue = (String) closeButton.getAttribute(DYNAMIC_ATTRIBUTE_NAME);
+
+        assertEquals(TAP_FAILED_MESSAGE, CANCEL_BUTTON_DYNAMIC_ATTRIBUTE_VALUE, dynamicAtributeValue);
+        assertEquals(ATTRIBUTE_VALUE_MISSMATCH_ERROR_MESSAGE,
+                     expectedResult[1],
+                     closeButton.getAttribute("name"));
+
     }
 }
