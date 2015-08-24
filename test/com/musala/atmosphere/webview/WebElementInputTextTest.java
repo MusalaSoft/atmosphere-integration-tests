906ac76e618f537104207b64140b87c8fa5e07bb
Yavor Stankov <yavor.stankov@musala.com>
2015-08-24 11:35:59 +0300

    Implement Input Text Method for Web Elements
    * add methods for focusing a web element and inputting text in web element.
    
    see #120
    
    Change-Id: Ia4f08b95b8ac19f4b4c2d4011e00f57a4fc5c18c

---
+++ test/com/musala/atmosphere/webview/WebElementInputTextTest.java
@@ -0,0 +1,68 @@
+package com.musala.atmosphere.webview;
+
+import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidatorAssert.setTestDevice;
+import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidatorAssert.startWebViewActivity;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.musala.atmosphere.BaseIntegrationTest;
+import com.musala.atmosphere.client.Screen;
+import com.musala.atmosphere.client.UiWebElement;
+import com.musala.atmosphere.client.WebView;
+import com.musala.atmosphere.commons.cs.deviceselection.DeviceSelector;
+import com.musala.atmosphere.commons.cs.deviceselection.DeviceSelectorBuilder;
+import com.musala.atmosphere.commons.cs.deviceselection.DeviceType;
+import com.musala.atmosphere.commons.webelement.selection.WebElementSelectionCriterion;
+
+/**
+ * 
+ * @author yavor.stankov
+ *
+ */
+public class WebElementInputTextTest extends BaseIntegrationTest {
+    private static final String INPUT_TEXT_ELEMENT_ID = "fname";
+
+    private static final String VALUE_ATTRIBUTE_KEY = "value";
+
+    private static final String TEXT_TO_INPUT = "Atmosphere";
+
+    private static Screen screen;
+
+    private static WebView webView;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        DeviceSelectorBuilder deviceSelectorBuilder = new DeviceSelectorBuilder();
+        DeviceSelector deviceSelector = deviceSelectorBuilder.deviceType(DeviceType.DEVICE_PREFERRED).build();
+
+        initTestDevice(deviceSelector);
+        setTestDevice(testDevice);
+
+        screen = testDevice.getActiveScreen();
+
+        startWebViewActivity();
+
+        webView = screen.getWebView(VALIDATOR_APP_PACKAGE);
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        testDevice.forceStopProcess(VALIDATOR_APP_PACKAGE);
+
+        releaseDevice();
+    }
+
+    @Test
+    public void testWebElementInputText() {
+        UiWebElement inputTextBox = webView.findElement(WebElementSelectionCriterion.ID, INPUT_TEXT_ELEMENT_ID);
+
+        assertTrue("Failed to input text in the selected web element.", inputTextBox.inputText(TEXT_TO_INPUT));
+        assertEquals("The input text is not the same as expected.",
+                     TEXT_TO_INPUT,
+                     inputTextBox.getAttribute(VALUE_ATTRIBUTE_KEY));
+    }
+}
