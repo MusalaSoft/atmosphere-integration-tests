8be9aaf2930763e56eb8971b849864187b9bc39d
Denis Bialev <denis.bialev@musala.com>
2015-10-06 16:04:35 +0300

    Add more testcases for isDisplayed
    Add more testcases for the new implementation of isDisplayed.
    Minor changes in the isDisplayed method.
    
    see #186
    
    Change-Id: Icc7b97e56dc57314516d378a2fdb7284ddf53207

--- test/com/musala/atmosphere/webview/WebElementIsDisplayedTest.java
+++ test/com/musala/atmosphere/webview/WebElementIsDisplayedTest.java
@@ -2,6 +2,7 @@ package com.musala.atmosphere.webview;
 
 import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidatorAssert.setTestDevice;
 import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidatorAssert.startWebViewActivity;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -28,6 +29,18 @@ public class WebElementIsDisplayedTest extends BaseIntegrationTest {
 
     private static final String HIDDEN_ELEMENT_ID = "hiddenCountry";
 
+    private static final String NEGATIVE_BOUNDS_ELEMENT_ID = "negativeBounds";
+
+    private static final String HIDDEN_ELEMENT_IS_DISPLAYED_FAIL_MESSAGE = "The hidden element was registered as displayed.";
+
+    private static final String EXPECTED_CSS_PROPERTY_FAIL_MESSAGE = "The found element does not contain the right CSS property.";
+
+    private static final String DISPLAY_ELEMENT_ID = "displayText";
+
+    private static final String VISIBIL_ELEMENT_ID = "visibilityText";
+
+    private static final String ZERO_OPACITY_ELEMENT_CLASS = "container-div";
+
     private static Screen screen;
 
     private static WebView webView;
@@ -53,14 +66,47 @@ public class WebElementIsDisplayedTest extends BaseIntegrationTest {
     }
 
     @Test
-    public void testIsWebElementDisplayed() {
+    public void testIsVisibleWebElementDisplayed() {
         UiWebElement webElement = webView.findElement(WebElementSelectionCriterion.ID, DISPLAYED_ELEMENT_ID);
         assertTrue("The displayed element was registered as hidden element.", webElement.isDisplayed());
     }
 
     @Test
-    public void testIsWebElementHidden() {
+    public void testIsHiddenWebElementDisplayed() {
         UiWebElement webElement = webView.findElement(WebElementSelectionCriterion.ID, HIDDEN_ELEMENT_ID);
-        assertFalse("The hidden element was registered as displayed element.", webElement.isDisplayed());
+        assertFalse(HIDDEN_ELEMENT_IS_DISPLAYED_FAIL_MESSAGE, webElement.isDisplayed());
+    }
+
+    @Test
+    public void testIsWebElementWithNegativeBoundsDisplayed() {
+        UiWebElement webElement = webView.findElement(WebElementSelectionCriterion.ID, NEGATIVE_BOUNDS_ELEMENT_ID);
+        assertFalse(HIDDEN_ELEMENT_IS_DISPLAYED_FAIL_MESSAGE, webElement.isDisplayed());
+    }
+
+    @Test
+    public void testIsWebElementDisplayedPropertyDisplay() {
+        UiWebElement webElement = webView.findElement(WebElementSelectionCriterion.ID, DISPLAY_ELEMENT_ID);
+        String expectedValue = "none";
+        String expectedProperty = "display";
+        assertEquals(EXPECTED_CSS_PROPERTY_FAIL_MESSAGE, expectedValue, webElement.getCssValue(expectedProperty));
+        assertFalse(HIDDEN_ELEMENT_IS_DISPLAYED_FAIL_MESSAGE, webElement.isDisplayed());
+    }
+
+    @Test
+    public void testIsWebElementDisplayedPropertyVisibility() {
+        UiWebElement webElement = webView.findElement(WebElementSelectionCriterion.ID, VISIBIL_ELEMENT_ID);
+        String expectedValue = "hidden";
+        String expectedProperty = "visibility";
+        assertEquals(EXPECTED_CSS_PROPERTY_FAIL_MESSAGE, expectedValue, webElement.getCssValue(expectedProperty));
+        assertFalse(HIDDEN_ELEMENT_IS_DISPLAYED_FAIL_MESSAGE, webElement.isDisplayed());
+    }
+
+    @Test
+    public void testIsWebElementWithZeroOpacityDisplayed() {
+        UiWebElement webElement = webView.findElement(WebElementSelectionCriterion.CLASS, ZERO_OPACITY_ELEMENT_CLASS);
+        String expectedValue = "0";
+        String expectedProperty = "opacity";
+        assertEquals(EXPECTED_CSS_PROPERTY_FAIL_MESSAGE, expectedValue, webElement.getCssValue(expectedProperty));
+        assertFalse(HIDDEN_ELEMENT_IS_DISPLAYED_FAIL_MESSAGE, webElement.isDisplayed());
     }
 }
