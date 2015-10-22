b9bb540ab41e7c82fa04c8d7f9ffe0b92db50309
yavor.stankov <yavor.stankov@musala.com>
2015-10-22 16:14:13 +0300

    Implement the Logic for Removing Screen Records
    Use the FileRecycler for removing the obsolete screen records, refactor some
    Unit and integration tests.
    
    see #213
    
    Change-Id: Ic14cd2835bdbb832fa5a8d46615100a256331d2d

--- test/com/musala/atmosphere/client/device/ScreenRecordingTest.java
+++ test/com/musala/atmosphere/client/device/ScreenRecordingTest.java
@@ -6,9 +6,7 @@ import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidato
 import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidatorAssert.startImeTestActivity;
 
 import java.io.File;
-import java.io.IOException;
 
-import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -30,19 +28,21 @@ import com.musala.atmosphere.test.util.ondevicevalidator.ContentDescriptor;
  * 
  */
 public class ScreenRecordingTest extends BaseIntegrationTest {
-    private static final String RECORDS_DIR_SUFFIX = "VideoRecords";
+    private static final String RECORDS_DIRECTORY_NAME = "ScreenRecords";
 
-    private static final String LOCAL_FILE_PATH = new File(".").getAbsolutePath();
+    private static final String RECORDS_DIRECTORY_PATH = new File(RECORDS_DIRECTORY_NAME).getAbsolutePath();
 
-    private static final long TIMEOUT_BETWEEN_INTERACTIONS = 40000;
+    private static final long TIMEOUT_BETWEEN_INTERACTIONS = 55000;
 
-    private static final int EXPECTED_RECORDED_FILES_COUNT = 2;
+    private static final int EXPECTED_RECORDED_FILES_COUNT = 1;
 
     private static final String EXPECTED_TEXT_RESULT = "Sample Text";
 
     private static final String COPY_BUTTON_CONTENT_DESCRIPTOR = "Copy";
 
     private static final String SELECT_ALL_BUTTON_CONTENT_DESCRIPTOR = "Select all";
+
+    private static File recordsDirectory;
 
     @BeforeClass
     public static void setUp() throws Exception {
@@ -51,19 +51,19 @@ public class ScreenRecordingTest extends BaseIntegrationTest {
         DeviceSelector testDeviceSelector = selectorBuilder.build();
         initTestDevice(testDeviceSelector);
         setTestDevice(testDevice);
-        startImeTestActivity();
 
-        removeOldRecords();
+        recordsDirectory = new File(RECORDS_DIRECTORY_PATH);
+        deleteDirectory(recordsDirectory);
+
+        startImeTestActivity();
     }
 
     @AfterClass
     public static void tearDown() throws Exception {
-        releaseDevice();
-    }
-
-    @After
-    public void tearDownTest() throws InterruptedException, IOException {
         testDevice.forceStopProcess(VALIDATOR_APP_PACKAGE);
+        deleteDirectory(recordsDirectory);
+
+        releaseDevice();
     }
 
     @Test
@@ -78,8 +78,6 @@ public class ScreenRecordingTest extends BaseIntegrationTest {
         copyTextBox.longPress();
 
         Thread.sleep(TIMEOUT_BETWEEN_INTERACTIONS);
-
-        screen.updateScreen();
 
         UiElementSelector selectAllButtonSelector = new UiElementSelector();
         selectAllButtonSelector.addSelectionAttribute(CssAttribute.CONTENT_DESCRIPTION,
@@ -103,8 +101,6 @@ public class ScreenRecordingTest extends BaseIntegrationTest {
 
         Thread.sleep(TIMEOUT_BETWEEN_INTERACTIONS);
 
-        screen.updateScreen();
-
         pasteTextBox = getElementByContentDescriptor(ContentDescriptor.EMPTY_TEXT_BOX.toString());
 
         assertElementText("Paste text failed! The text field content does not match the expected one.",
@@ -113,48 +109,26 @@ public class ScreenRecordingTest extends BaseIntegrationTest {
 
         testDevice.stopScreenRecording();
 
-        File screenRecordsDirectory = null;
-        int actualRecordedFilesCount = 0;
-        File currentDir = new File(LOCAL_FILE_PATH);
+        Assert.assertTrue("The screen records folder does not exist.", recordsDirectory.exists());
 
-        for (String file : currentDir.list()) {
-            if (file.contains(RECORDS_DIR_SUFFIX)) {
-                screenRecordsDirectory = new File(LOCAL_FILE_PATH + File.separator + file);
-            }
-        }
-
-        if (screenRecordsDirectory != null) {
-            String[] recordedFiles = screenRecordsDirectory.list();
-            actualRecordedFilesCount = recordedFiles.length;
-        }
+        int actualRecordedFilesCount = recordsDirectory.list().length;
 
         Assert.assertEquals("The number of recorded files is not the same as the expected!",
                             EXPECTED_RECORDED_FILES_COUNT,
                             actualRecordedFilesCount);
     }
 
-    // TODO: This should be executed on Agent stop.
-    private static void removeOldRecords() {
-        File currentDir = new File(LOCAL_FILE_PATH);
-        for (String file : currentDir.list()) {
-            if (file.contains(RECORDS_DIR_SUFFIX)) {
-                deleteDirectory(new File(LOCAL_FILE_PATH + File.separator + file));
-            }
-        }
-    }
-
     private static boolean deleteDirectory(File directory) {
         if (directory.exists()) {
             File[] files = directory.listFiles();
 
-            if (files != null) {
-
-                for (File file : files) {
-                    file.delete();
-                }
+            for (File file : files) {
+                file.delete();
             }
+
+            return directory.delete();
         }
 
-        return (directory.delete());
+        return false;
     }
 }
