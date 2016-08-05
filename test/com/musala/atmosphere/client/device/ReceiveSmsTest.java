06fad8544de894c572d4d22abef7cdac135a1461
Aleksander Ivanov <aleksander.ivanov@musala.com>
2016-08-05 18:40:11 +0300

    Fix ReceiveSmsTest
    Fix ReceiveSmsTest when ran on a physical device.

--- test/com/musala/atmosphere/client/device/ReceiveSmsTest.java
+++ test/com/musala/atmosphere/client/device/ReceiveSmsTest.java
@@ -5,6 +5,7 @@ import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidato
 import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidatorAssert.setTestDevice;
 import static com.musala.atmosphere.test.util.ondevicevalidator.OnDeviceValidatorAssert.startMainActivity;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeNotNull;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -16,6 +17,7 @@ import com.musala.atmosphere.commons.beans.PhoneNumber;
 import com.musala.atmosphere.commons.cs.deviceselection.DeviceSelector;
 import com.musala.atmosphere.commons.cs.deviceselection.DeviceSelectorBuilder;
 import com.musala.atmosphere.commons.cs.deviceselection.DeviceType;
+import com.musala.atmosphere.commons.exceptions.NoAvailableDeviceFoundException;
 
 public class ReceiveSmsTest extends BaseIntegrationTest {
     private static final String SENDER_PHONE = "+012345";
@@ -26,23 +28,29 @@ public class ReceiveSmsTest extends BaseIntegrationTest {
     public static void setUp() throws Exception {
         DeviceSelectorBuilder selectorBuilder = new DeviceSelectorBuilder().deviceType(DeviceType.EMULATOR_ONLY);
         DeviceSelector testDeviceSelector = selectorBuilder.build();
-        initTestDevice(testDeviceSelector);
 
-        setTestDevice(testDevice);
+        try {
+            initTestDevice(testDeviceSelector);
+            setTestDevice(testDevice);
 
-        startMainActivity();
-
-        assertValidatorIsStarted();
+            startMainActivity();
+            assertValidatorIsStarted();
+        } catch (NoAvailableDeviceFoundException e) {
+            // Nothing to do here
+        }
     }
 
     @AfterClass
     public static void tearDown() throws Exception {
-        testDevice.forceStopProcess(VALIDATOR_APP_PACKAGE);
+        if (testDevice != null) {
+            testDevice.forceStopProcess(VALIDATOR_APP_PACKAGE);
+        }
         releaseDevice();
     }
 
     @Test
     public void testReceiveSms() throws Exception {
+        assumeNotNull(testDevice);
         PhoneNumber phoneNumber = new PhoneNumber(SENDER_PHONE);
         SmsMessage sms = new SmsMessage(phoneNumber, SMS_TEXT);
         assertTrue("Sending SMS returned false.", testDevice.receiveSms(sms));
